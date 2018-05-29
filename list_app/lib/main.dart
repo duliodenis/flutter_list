74ee3d530d1f82555da16030a5eca3d12b14b47e
Dulio Denis <dulio.denis@yahoo.com>
2018-05-29 16:01:35 -0400

    Added a multi-row Container in a ListView in the HomeWidget

--- list_app/lib/main.dart
+++ list_app/lib/main.dart
@@ -20,8 +20,22 @@ class ListApp extends StatelessWidget {
 class HomeWidget extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
-      return Center(
-        child: Text("Hello, World!", style: TextStyle(fontSize: 36.0))
+      return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, row) {
+          return Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+            children: <Widget>[
+              Image.network("https://i.ytimg.com/vi/HitSIzPM_6E/maxresdefault.jpg"),
+              Container(height: 8.0,),
+              Text("Learn iOS development from the master", 
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
+              Divider(color: Colors.green)
+              ],
+            )
+          );
+        },
       );
     }
 }
